9e0dcaab353fca1e84209dbb1076b6b9197abfed
nrohr <nick@rstudio.com>
2020-09-15 15:08:48 +0000

    changing Shiny app title

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("This is a New Title"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
